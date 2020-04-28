b86d74d9ab1a2ce1ca324183b0ad9850c45f3a30
j-davies <joachim.davies@gmail.com>
2020-04-28 12:43:22 +0200

    disable-dev-shm-usage

--- src/rendertron.ts
+++ src/rendertron.ts
@@ -23,7 +23,7 @@ export class Rendertron {
   private host = process.env.HOST || this.config.host;
 
   async createRenderer(config: Config) {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox ', '--disable-dev-shm-usage'] });
 
     browser.on('disconnected', () => {
       this.createRenderer(config);
